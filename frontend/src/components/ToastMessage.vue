0059a1854016dbbb9fd5687b6b72d9f2c69004f2
CLAUDIU STEFAN <stefanclaudiu@programmer.net>
2023-11-27 14:44:50 +0000

    implementing a Toast Message for error visualization

--- frontend/src/components/ToastMessage.vue
+++ frontend/src/components/ToastMessage.vue
@@ -1,55 +1,48 @@
 <template>
-    <div class="toast-message col-4">
-
-        <EarthBoyIllustration />
-        <h4 v-text="title"></h4>
-        <h5 v-text="message"></h5>
-    </div>
-
-    
+  <div class="toast-message col-4">
+    <EarthBoyIllustration />
+    <h4 v-text="title"></h4>
+    <h5 v-text="message"></h5>
+  </div>
 </template>
 
 <script>
-import EarthBoyIllustration from './illustrations/EarthBoyIllustration.vue';
-
+import EarthBoyIllustration from "./illustrations/EarthBoyIllustration.vue";
 
 export default {
-    name: "ToastMessage",
-    components:{
-        EarthBoyIllustration 
-    },
-    props:{
-        title: String,
-        message: String
-
-
-    }
-}
-
+  name: "ToastMessage",
+  components: {
+    EarthBoyIllustration,
+  },
+  props: {
+    title: String,
+    message: String,
+  },
+};
 </script>
 
 <style scoped>
-
-.toast-message{
-    display: flex;
-    min-height: 450px;
-    background: #fff;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+.toast-message {
+  display: flex;
+  min-height: 450px;
+  background: #fff;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 }
 
-.toast-message h4{
-    font-family: 'Kanit', sans-serif;
-    font-size: 2em;
-    margin-bottom: 15px;
+.toast-message h4 {
+  font-family: "Kanit", sans-serif;
+  font-size: 1.7em;
+  margin-bottom: 15px;
+  margin-top: 20px;
 }
 
-.toast-message h5{
-    font-family: 'Kanit', sans-serif;
-    font-family: 'Montserrat', sans-serif;
-    color: rgb(117, 117, 117);
-    font-size: 1.5em;
+.toast-message h5 {
+  font-family: "Kanit", sans-serif;
+  font-family: "Montserrat", sans-serif;
+  color: rgb(117, 117, 117);
+  font-size: 1.1em;
 }
-
 </style>
